a6a6345533e6fd11d58393a29051e1444182a362
Willem Meints <willem@mein-design.nl>
2016-11-16 09:51:22 +0100

    Add solution structure

---
+++ src/GithubAutomation.Webhooks/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using GithubAutomation.Webhooks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace GithubAutomation.Webhooks
+{
+    /// <summary>
+    /// The startup logic for the web application
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Configures the request pipeline for the application
+        /// </summary>
+        /// <param name="app"></param>
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWebApi(CreateHttpConfiguration());
+        }
+
+        private HttpConfiguration CreateHttpConfiguration()
+        {
+            var config = new HttpConfiguration();
+
+            config.InitializeReceiveGitHubWebHooks();
+
+            return config;
+        }
+    }
+}
---
+++ src/GithubAutomation.Webhooks/WebHooks/GithubWebhookHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNet.WebHooks;
+using Newtonsoft.Json.Linq;
+
+namespace GithubAutomation.Webhooks.WebHooks
+{
+    /// <summary>
+    /// Handles incoming webhook calls from github
+    /// </summary>
+    public class GithubWebhookHandler: WebHookHandler
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="GithubWebhookHandler"/>
+        /// </summary>
+        public GithubWebhookHandler()
+        {
+            this.Receiver = GitHubWebHookReceiver.ReceiverName;
+        }
+
+        /// <summary>
+        /// Executes the webhook handler
+        /// </summary>
+        /// <param name="receiver"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override Task ExecuteAsync(string receiver, WebHookHandlerContext context)
+        {
+            // For more information about GitHub WebHook payloads, please see 
+            // 'https://developer.github.com/webhooks/'
+            JObject entry = context.GetDataOrDefault<JObject>();
+
+            Trace.WriteLine("Received call from github");
+
+            return Task.FromResult(true);
+        }
+    }
+}
